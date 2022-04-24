1c95c3eabb057b64e127d242476185f148f389b8
Robatortas <robatortas1@gmail.com>
2022-04-24 00:46:01 -0700

    I AM STILL ALIVE OK? I just forget to commit sometimes

--- src/robatortas/code/files/Downloader.java
+++ src/robatortas/code/files/Downloader.java
@@ -1,8 +1,13 @@
 package robatortas.code.files;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
@@ -13,6 +18,7 @@ public class Downloader {
 	
 	private Scanner readInput = new Scanner(System.in);
 	private String input;
+	private String dir;
 	private String inputLink;
 	private String inputFileName;
 	
@@ -43,7 +49,7 @@ public class Downloader {
 		}
 		System.out.println("What kind of download do you want to compute?" + "\n");
 		
-		System.out.println("-Option A: Directory" + "\n");
+		System.out.println("-Option A: Directory (STILL IN DEVELOPMENT!)" + "\n");
 		System.out.println("-Option B: File" + "\n");
 		
 		System.out.println("Please type out your answer..." + "\n");
@@ -56,31 +62,92 @@ public class Downloader {
 		this.input = readInput.nextLine().toLowerCase();
 		
 		// I could use switch case, but I am lazy as fuck.
+		// And the code could be much more optimized, but I am lazyyyY!
 		if(input.contains("a".toLowerCase())) {
 			System.out.println("\n" + "You'll be downloading files through a DIRECTORY it seems.");
-			computeDirDownload();
-		}
-		if(input.contains("b".toLowerCase())) {
-			System.out.println("\n" + "You'll be downloading a FILE you say." + "\n");
-			System.out.println("Input the download link:" + "\n");
-			
+
+			System.out.println("\n" + "Input the download link:" + "\n");
 			this.inputLink = readInput.nextLine().toLowerCase();
 			
-			System.out.println("\n" + "Input the file name" + "\n");
+			System.out.println("\n" + "Input the saving location on your machine" + "\n");
+			
+			this.dir = readInput.nextLine().toLowerCase();
+			
+			System.out.println("Now input the file name (with its extention (Ex: myFile.jpg))");
+			
 			this.inputFileName = readInput.nextLine().toLowerCase();
-			// TODO: CONVERT INPUTLINK TO A URL!!!
-//			computeFileDownload(inputLink, inputFileName);
+			
+			String savingLocation = dir + "\\" + inputFileName;
+			
+			try {
+				computeDirDownload(new URL(inputLink), inputFileName);
+				System.out.println(checkFileSize(new URL(inputLink)));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		if(input.contains("b".toLowerCase())) {
+			System.out.println("Input the download link:" + "\n");
+		
+			this.inputLink = readInput.nextLine().toLowerCase();
+			
+			System.out.println("\n" + "Input the saving location on your machine" + "\n");
+			
+			this.dir = readInput.nextLine().toLowerCase();
+			
+			System.out.println("Now input the file name (with its extention (Ex: myFile.jpg))");
+			
+			this.inputFileName = readInput.nextLine().toLowerCase();
+			
+			String savingLocation = dir + "\\" + inputFileName;
+		
+			try {
+				computeFileDownload(new URL(inputLink), savingLocation);
+				System.out.println(checkFileSize(new URL(inputLink)) + "bytes");
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
 	// Computes the DIRECTORY download
-	public void computeDirDownload() {
-		
+	public void computeDirDownload(URL url, String fileName) {
+		 byte[] buffer = new byte[1024];
+		 InputStream inP = null;
+		try { inP = url.openStream();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		    int bytesRead = -1;
+		    StringBuilder page = new StringBuilder(1024);
+		    try {
+				while ((bytesRead = inP.read(buffer)) != -1) {
+				    page.append(new String(buffer, 0, bytesRead));
+				    Files.copy(inP, Paths.get(dir + "\\" + fileName));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+	
+	public int checkFileSize(URL url) {
+		try {
+			URLConnection connection = url.openConnection();
+			connection.connect();
+			int fileSize = connection.getContentLength();
+			return fileSize;
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("Exception: URLConnection error");
+		}
+		return 0;
 	}
 	
 	// Computes the FILE download
 	public void computeFileDownload(URL url, String fileName) {
 		try (InputStream inP = url.openStream()){
+			// Copies bytes from the inputStream to a file
 			Files.copy(inP, Paths.get(fileName));
 		} catch (IOException e) {
 			System.err.println("EXCEPTION: inP != url.openStream()");
